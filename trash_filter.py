8fd1fd34af96aee873143467c0490e1ab835058c
Alexey Kott <alexey.kott@gmail.com>
2018-04-18 19:19:39 +0300

    Filter for channel compilation

--- trash_filter.py
+++ trash_filter.py
@@ -1,8 +1,23 @@
+import re
+
+
 def is_trash(msg_text):
-    trash = ['подписыва', 'подписаться']
     msg_text = msg_text.lower()
+
+    # если алиасов больше 4-х, то скорее всего это подборка каналов
+    if len(count_aliases(msg_text)) > 4: 
+    	return True
+
+    # спам-слова
+    trash = ['подписыва', 'подписаться']
     for word in trash:
         if msg_text.find(word) != -1:
             return True
 
     return False
+
+
+def count_aliases(msg_text):
+	aliases = re.findall(r'@[\S]+', msg_text)
+
+	return len(aliases)
