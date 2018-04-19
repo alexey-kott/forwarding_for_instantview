7bc040ad672d38dd410d67eb5dcc35ded626aa3f
Alexey Kott <alexey.kott@gmail.com>
2018-04-19 16:13:09 +0300

    Spaces to tab

--- main.py
+++ main.py
@@ -10,34 +10,34 @@ from config import TG_API_ID, TG_API_HASH, TG_APP_TITLE, PHONE, FORWARDING_CHANN
 
 
 def main():
-    client = TelegramClient(PHONE.strip('+'),
-                            TG_API_ID,
-                            TG_API_HASH,
-                            proxy=None,
-                            update_workers=4,
-                            spawn_read_thread=False)
+	client = TelegramClient(PHONE.strip('+'),
+							TG_API_ID,
+							TG_API_HASH,
+							proxy=None,
+							update_workers=4,
+							spawn_read_thread=False)
 
-    client.start()
+	client.start()
 
-    if not client.is_user_authorized():
-        client.send_code_request(PHONE)
-        client.sign_in(PHONE, input("Enter code: "))
+	if not client.is_user_authorized():
+		client.send_code_request(PHONE)
+		client.sign_in(PHONE, input("Enter code: "))
 
-    @client.on(events.NewMessage)
-    def handle_msg(event):
-        if event.is_channel:
+	@client.on(events.NewMessage)
+	def handle_msg(event):
+		if event.is_channel:
 
-            channel = client.get_entity(event.message.to_id)
-            if channel.username in FORWARDING_CHANNELS:
-                msg_text = event.message.message
-                
-                # if is_trash(msg_text):
-                #     return
-                
-                client.send_message(DEST_CHANNEL, msg_text, file=event.message.media)
+			channel = client.get_entity(event.message.to_id)
+			if channel.username in FORWARDING_CHANNELS:
+				msg_text = event.message.message
+				
+				# if is_trash(msg_text):
+				#     return
+				
+				client.send_message(DEST_CHANNEL, msg_text, file=event.message.media)
 
-    client.idle()
+	client.idle()
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- trash_filter.py
+++ trash_filter.py
@@ -2,19 +2,19 @@ import re
 
 
 def is_trash(msg_text):
-    msg_text = msg_text.lower()
+	msg_text = msg_text.lower()
 
-    # если алиасов больше 4-х, то скорее всего это подборка каналов
-    if len(count_aliases(msg_text)) > 4: 
-    	return True
+	# если алиасов больше 4-х, то скорее всего это подборка каналов
+	if len(count_aliases(msg_text)) > 4: 
+		return True
 
-    # спам-слова
-    trash = ['подписыва', 'подписаться']
-    for word in trash:
-        if msg_text.find(word) != -1:
-            return True
+	# спам-слова
+	trash = ['подписыва', 'подписаться']
+	for word in trash:
+		if msg_text.find(word) != -1:
+			return True
 
-    return False
+	return False
 
 
 def count_aliases(msg_text):
