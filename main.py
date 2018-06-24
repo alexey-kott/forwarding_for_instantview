80f8e4f094839e8df6f0cafbf3522e12b5626860
Alexey Kott <alexey.kott@gmail.com>
2018-06-24 17:19:13 +0300

    Added translator

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from collections import defaultdict
 
 from telethon import TelegramClient, events
+from googletrans import Translator
 
-from config import TG_API_ID, TG_API_HASH, PHONE, FORWARDING_SCHEMA
+from config import TG_API_ID, TG_API_HASH, PHONE
+from forwarding_schema import FORWARDING_SCHEMA
 import logging
+
 logging.basicConfig(level=logging.ERROR)
+translator = Translator()
 
 
 def get_forwarding_schema():
@@ -14,6 +18,8 @@ def get_forwarding_schema():
         for direction, names in forwarding_item.items():
             entities = {search_entities(name) for name in names if search_entities(name)}
             item[direction] = entities
+        if forwarding_item.get("TRANSLATE"):
+            item['TRANSLATE'] = forwarding_item['TRANSLATE']
         forwarding_schema.append(item)
 
     return forwarding_schema
@@ -35,6 +41,16 @@ def get_dialog_by_id(entity_id):
     for dialog in user_dialogs:
         if dialog.entity.id == entity_id:
             return dialog.entity
+
+
+def get_message_text(addressee, msg_text, translate_schema=None):
+    if translate_schema is None:
+        return f"{addressee}\n {msg_text}"
+    else:
+        translate = translator.translate(msg_text,
+                                         src=translate_schema.get("FROM"),
+                                         dest=translate_schema.get('TO'))
+        return f"{addressee}\n {translate.text}"
 
 
 def main():
@@ -72,7 +88,10 @@ def main():
                 except TypeError:
                     addressee_name = f"{entity.title}"
 
-                msg_text = f"{addressee_name}\n {event.message.message}"
+                msg_text = get_message_text(addressee=addressee_name,
+                                            msg_text=event.message.message,
+                                            translate_schema=item.get('TRANSLATE'))
+
                 for dest_id in item['DESTINATION']:
                     client.send_message(dest_id, msg_text, file=event.message.media)
 
