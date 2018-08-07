3513885cbdeab2dd266bccdae6daa2539783e54b
Alexey Kott <alexey.kott@gmail.com>
2018-08-07 17:42:35 +0300

    Moved to async

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import asyncio
 from collections import defaultdict
+from typing import Union
 
 from telethon import TelegramClient, events
+from telethon.tl.types import PeerChannel, PeerChat, PeerUser
 from googletrans import Translator
 
 from config import TG_API_ID, TG_API_HASH, PHONE
@@ -9,10 +12,14 @@ import logging
 
 logging.basicConfig(level=logging.ERROR)
 translator = Translator()
+forwarding_schema = []
+
+client = TelegramClient(PHONE.strip('+'),
+                        TG_API_ID,
+                        TG_API_HASH)
 
 
-def get_forwarding_schema():
-    forwarding_schema = []
+def init_forwarding_schema():
     for forwarding_item in FORWARDING_SCHEMA:
         item = defaultdict(set)
         for direction, names in forwarding_item.items():
@@ -22,10 +29,8 @@ def get_forwarding_schema():
             item['TRANSLATE'] = forwarding_item['TRANSLATE']
         forwarding_schema.append(item)
 
-    return forwarding_schema
 
-
-def search_entities(name):
+def search_entities(name: str) -> int:
     for dialog in user_dialogs:
         if name.startswith('@'):
             if getattr(dialog.entity, 'username', None) == name.strip('@'):
@@ -37,7 +42,18 @@ def search_entities(name):
                 return dialog.entity.id
 
 
-def get_dialog(peer_entity):
+def get_message_text(addressee: str,
+                     msg_text: str, translate_schema=None) -> str:
+    if translate_schema is None:
+        return f"{msg_text}"
+    else:
+        translate = translator.translate(msg_text,
+                                         src=translate_schema.get("FROM"),
+                                         dest=translate_schema.get('TO'))
+        return f"{addressee}\n {msg_text}\n\n{translate.text}"
+
+
+def get_dialog(peer_entity: Union[PeerChannel, PeerChat, PeerUser]):
     try:
         entity_id = peer_entity.channel_id
     except AttributeError:
@@ -48,60 +64,41 @@ def get_dialog(peer_entity):
             return dialog.entity
 
 
-def get_message_text(addressee, msg_text, translate_schema=None):
-    if translate_schema is None:
-        return f"{msg_text}"
-    else:
-        translate = translator.translate(msg_text,
-                                         src=translate_schema.get("FROM"),
-                                         dest=translate_schema.get('TO'))
-        return f"{addressee}\n {msg_text}\n\n{translate.text}"
+@client.on(events.NewMessage)
+async def handler(event):
+    sender_ids = {entity.id for entity_id, entity in event._entities.items()}
+
+    for item in forwarding_schema:
+        intersection = item['SOURCE'] & sender_ids
+        if intersection:
+            entity = get_dialog(event.message.to_id)
+            try:
+                addressee = await client.get_entity(event.message.from_id)
+                last_name = lambda name: name if None else ''
+                addressee_name = f"{addressee.first_name} {last_name(addressee.last_name)}"
+            except TypeError:
+                addressee_name = f"{entity.title}"
+
+            msg_text = get_message_text(addressee=addressee_name,
+                                        msg_text=event.message.message,
+                                        translate_schema=item.get('TRANSLATE'))
+
+            for dest_id in item['DESTINATION']:
+                await client.send_message(dest_id, msg_text, file=event.message.media)
 
 
-def main():
-    global client
+async def main() -> None:
     global user_dialogs
-    client = TelegramClient(PHONE.strip('+'),
-                            TG_API_ID,
-                            TG_API_HASH,
-                            proxy=None,
-                            update_workers=4,
-                            spawn_read_thread=False)
 
-    client.start()
+    await client.start()
 
-    if not client.is_user_authorized():
-        client.send_code_request(PHONE)
-        client.sign_in(PHONE, input("Enter code: "))
+    user_dialogs = await client.get_dialogs()
+    init_forwarding_schema()
 
-    user_dialogs = client.get_dialogs()
-    forwarding_schema = get_forwarding_schema()
-
-    @client.on(events.NewMessage)
-    def handle_msg(event):
-
-        sender_ids = {entity.id for id, entity in event._entities.items()}
-
-        for item in forwarding_schema:
-            intersection = item['SOURCE'] & sender_ids
-            if intersection:
-                entity = get_dialog(event.message.to_id)
-                try:
-                    addressee = client.get_entity(event.message.from_id)
-                    last_name = lambda name: name if None else ''
-                    addressee_name = f"{addressee.first_name} {last_name(addressee.last_name)}"
-                except TypeError:
-                    addressee_name = f"{entity.title}"
-
-                msg_text = get_message_text(addressee=addressee_name,
-                                            msg_text=event.message.message,
-                                            translate_schema=item.get('TRANSLATE'))
-
-                for dest_id in item['DESTINATION']:
-                    client.send_message(dest_id, msg_text, file=event.message.media)
-
-    client.idle()
+    await client.run_until_disconnected()
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
